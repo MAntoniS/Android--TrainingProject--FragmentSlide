65df6cba9408cc699d185c868582c7980f009cdf
MAntoniS <sobota.antoni@gmail.com>
2020-04-04 15:11:43 -0700

    Sliding between Fragments

--- app/src/main/java/com/example/fragmentslideapp/MainActivity.java
+++ app/src/main/java/com/example/fragmentslideapp/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.fragmentslideapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    private static final int NUM_PAGES = 5;
+
+    private ViewPager mPager;
+
+    private PagerAdapter mPagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+
+
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+        public ScreenSlidePagerAdapter(FragmentManager fragmentManager){
+            super(fragmentManager);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return new ScreenSlidePageFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
